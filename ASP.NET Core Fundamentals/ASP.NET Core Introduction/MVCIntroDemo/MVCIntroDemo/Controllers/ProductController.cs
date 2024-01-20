e8437e330e741b9eaf567e7e1b628af6a5b5dcb4
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-01-20 19:35:01 +0200

    ASP.NET Core Exercise

---
+++ ASP.NET Core Fundamentals/ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Controllers/ProductController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVCIntroDemo.Models.Product;
+
+namespace MVCIntroDemo.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly ILogger  _logger;
+
+		private IEnumerable<ProductViewModel> _products
+			= new List<ProductViewModel>()
+			{
+				new ProductViewModel()
+				{
+					Id = 1,
+					Name = "Cheese",
+					Price = 7.00
+				},
+				new ProductViewModel()
+				{
+					Id = 2,
+					Name = "Ham",
+					Price = 5.50
+				},
+				new ProductViewModel()
+				{
+					Id = 3,
+					Name = "Bread",
+					Price = 2.20
+				},
+			};
+
+		public ProductController(ILogger<ProductController> logger)
+		{
+			_logger = logger;
+		}
+
+		public IActionResult Index()
+		{
+			return View(_products);
+		}
+
+		public IActionResult ById(int id)
+		{
+			var product = _products.FirstOrDefault(p => p.Id == id);
+
+			if (product == null)
+			{
+				return BadRequest();
+			}
+
+			return View(product);
+		}
+	}
+}
---
+++ ASP.NET Core Fundamentals/ASP.NET Core Introduction/MVCIntroDemo/MVCIntroDemo/Models/Product/ProductViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MVCIntroDemo.Models.Product
+{
+	public class ProductViewModel
+	{
+		/// <summary>
+		/// Id product.
+		/// </summary>
+		public int Id { get; set; }
+
+		/// <summary>
+		/// Name of product.
+		/// </summary>
+		public string Name { get; set; } = string.Empty;
+
+
+		/// <summary>
+		/// Price of the product.
+		/// </summary>
+		public double Price { get; set; }
+	}
+}
