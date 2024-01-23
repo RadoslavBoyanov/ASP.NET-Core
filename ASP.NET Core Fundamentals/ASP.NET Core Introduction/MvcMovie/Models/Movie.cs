1ae31bd730b6a62c86949efbd3f6e06f55d7f7a4
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-01-23 12:24:35 +0200

    Edit layout in MVCMovie

--- ASP.NET Core Fundamentals/ASP.NET Core Introduction/MvcMovie/Models/Movie.cs
+++ ASP.NET Core Fundamentals/ASP.NET Core Introduction/MvcMovie/Models/Movie.cs
@@ -9,23 +9,23 @@ namespace MvcMovie.Models
 
 		//[StringLength(60), MinLength(3)]
 		//[Required]
-		public string? Title { get; set; }
+		public string? Title { get; set; } = string.Empty;
 
-		[Display(Name = "Release Date")]
+        [Display(Name = "Release Date")]
 		[DataType(DataType.Date)]
 		public DateTime ReleaseDate { get; set; }
 
-		//[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
-		//[Required]
-		//[StringLength(30)]
-		public string? Genre { get; set; }
+        //[RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        //[Required]
+        //[StringLength(30)]
+        public string? Genre { get; set; } = string.Empty;
 
 		[Column(TypeName = "decimal(18,2)")]
 		public decimal Price { get; set; }
 
-        //[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
-        //[StringLength(5)]
-        //[Required]
-        public string? Rating { get; set; }
+		//[RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
+		//[StringLength(5)]
+		//[Required]
+		public string? Rating { get; set; } = string.Empty;
     }
 }
