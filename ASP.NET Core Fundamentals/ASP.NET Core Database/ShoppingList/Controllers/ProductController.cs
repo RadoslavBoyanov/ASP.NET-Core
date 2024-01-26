24883f6e105a90c4fd8a2fa720466ce5b2760eac
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-01-26 12:32:46 +0200

    Finishing my exercise with CRUD operations on Shopping list APP

--- ASP.NET Core Fundamentals/ASP.NET Core Database/ShoppingList/Controllers/ProductController.cs
+++ ASP.NET Core Fundamentals/ASP.NET Core Database/ShoppingList/Controllers/ProductController.cs
@@ -19,5 +19,49 @@ namespace ShoppingList.Controllers
 
             return View(allproducts);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(ProductViewModel newProduct)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            await this.productService.AddProductAsync(newProduct);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int id)
+        {
+            var productViewModel = await this.productService.GetProductByIdAsync(id);
+            return View(productViewModel);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(int id, ProductViewModel product)
+        {
+            if (!ModelState.IsValid || product.Id != id)
+            {
+                return View();
+            }
+
+            await productService.UpdateProductAsync(id, product);
+            return RedirectToAction(nameof(Index));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await this.productService.DeleteProductAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
     }
 }
--- ASP.NET Core Fundamentals/ASP.NET Core Database/ShoppingList/Services/ProductService.cs
+++ ASP.NET Core Fundamentals/ASP.NET Core Database/ShoppingList/Services/ProductService.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using ShoppingList.Contracts;
 using ShoppingList.Data;
+using ShoppingList.Data.Models;
 using ShoppingList.Models;
 
 namespace ShoppingList.Services
@@ -14,9 +15,21 @@ namespace ShoppingList.Services
             _dbContext = context;
         }
 
+        public async Task AddProductAsync(ProductViewModel product)
+        {
+            var entity = new Product()
+            {
+                Name = product.Name,
+            };
+
+            await _dbContext.Products.AddAsync(entity);
+            await _dbContext.SaveChangesAsync();
+        }
+
         public Task<List<ProductViewModel>> GetAllProductsAsync()
         {
             return _dbContext.Products
+                .AsNoTracking()
                 .Select(p => new ProductViewModel
                 {
                     Id = p.Id,
@@ -24,24 +37,46 @@ namespace ShoppingList.Services
                 }).ToListAsync();
         }
 
-        public Task AddProductAsync(ProductViewModel product)
+        public async Task UpdateProductAsync(int id, ProductViewModel product)
         {
-            throw new NotImplementedException();
+            var entity = await _dbContext.Products.FindAsync(product.Id);
+
+            if (entity is null)
+            {
+                throw new ArgumentException("Invalid product!");
+            }
+
+            entity.Name = product.Name;
+            await _dbContext.SaveChangesAsync();
         }
 
-        public Task UpdateProductAsync(int id, ProductViewModel product)
+        public async Task DeleteProductAsync(int id)
         {
-            throw new NotImplementedException();
+            var entity = await _dbContext.Products.FindAsync(id);
+
+            if (entity is null)
+            {
+                throw new ArgumentException("Invalid product!");
+            }
+
+            _dbContext.Products.Remove(entity);
+            await _dbContext.SaveChangesAsync();
         }
 
-        public Task DeleteProductAsync(int id)
+        public async Task<ProductViewModel> GetProductByIdAsync(int id)
         {
-            throw new NotImplementedException();
-        }
+            var entity = await _dbContext.Products.FindAsync(id);
 
-        public Task<ProductViewModel> GetProductByIdAsync(int id)
-        {
-            throw new NotImplementedException();
+            if (entity is null)
+            {
+                throw new ArgumentException("Invalid product!");
+            }
+
+            return new ProductViewModel()
+            {
+                Id = id,
+                Name = entity.Name,
+            };
         }
     }
 }
