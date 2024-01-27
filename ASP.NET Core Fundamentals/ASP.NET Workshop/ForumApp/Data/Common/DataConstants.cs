55b026981df7782bd5abeae8e6930b57cd449074
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-01-27 17:20:35 +0200

    ForumApp - workshop

---
+++ ASP.NET Core Fundamentals/ASP.NET Workshop/ForumApp/Data/Common/DataConstants.cs
@@ -0,0 +1,13 @@
+﻿namespace ForumApp.Data.Common
+{
+    public class DataConstants
+    {
+        public class Post
+        {
+            public const int TitleMaxLength = 50;
+            public const int TitleMinLength = 10;
+            public const int ContentMaxLength = 1500;
+            public const int ContentMinLength = 10;
+        }
+    }
+}
---
+++ ASP.NET Core Fundamentals/ASP.NET Workshop/ForumApp/Data/Migrations/20240127134019_CreatedTables.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ForumApp.Data.Migrations
+{
+    public partial class CreatedTables : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Posts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Content = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: false),
+                    IsDelete = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Posts", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "IsDelete", "Title" },
+                values: new object[] { 1, "My archeology education.", false, "My first Post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "IsDelete", "Title" },
+                values: new object[] { 2, "My education in Software University.", false, "My second post" });
+
+            migrationBuilder.InsertData(
+                table: "Posts",
+                columns: new[] { "Id", "Content", "IsDelete", "Title" },
+                values: new object[] { 3, "Books we should read.", false, "My third post" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Posts");
+        }
+    }
+}
