f797e9355b9c30532ad1f2d665e3c19a8e1e9a1e
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-02-07 20:44:53 +0200

    Add ForumApp project

---
+++ ASP.NET Core Fundamentals/ASP.NET Core Workshop Forum App/ForumApp/Contracts/IPostService.cs
@@ -0,0 +1,16 @@
+﻿using ForumApp.Models;
+
+namespace ForumApp.Contracts
+{
+	public interface IPostService
+	{
+		public Task<List<PostViewModel>> GetAllAsync();
+		public Task AddPostAsync(PostFormModel postViewModel);
+
+		public Task<PostFormModel> FindByIdAsync(int id);
+
+		public Task EditPostAsync(int id, PostFormModel postViewModel);
+
+		public Task DeletePostAsync(int id);
+	}
+}
---
+++ ASP.NET Core Fundamentals/ASP.NET Core Workshop Forum App/ForumApp/Models/PostFormModel.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ForumApp.Models
+{
+	using ForumApp.Data.Common;
+	public class PostFormModel
+	{
+		[Required]
+		[StringLength(DataConstants.Post.TitleMaxLength, MinimumLength = DataConstants.Post.TitleMinLength)]
+		public string Title { get; set; } = string.Empty;
+
+		[Required]
+		[StringLength(DataConstants.Post.ContentMaxLength, MinimumLength = DataConstants.Post.ContentMinLength)]
+		public string Content { get; set; } = string.Empty;
+	}
+}
