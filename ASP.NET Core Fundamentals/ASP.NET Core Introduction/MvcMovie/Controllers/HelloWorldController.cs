a89a67f956f6371da313d992557f51a60fb60779
Radoslav Boyanov <106109182+RadoslavBoyanov@users.noreply.github.com>
2024-02-07 21:04:27 +0200

    Add MvcMovie from tutorial in https://learn.microsoft.com/

---
+++ ASP.NET Core Fundamentals/ASP.NET Core Introduction/MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: /HelloWorld/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        //GET : /HelloWorld/Welcome/
+        //Requires using System.Text.Encodings.Web
+        public IActionResult Welcome(string name,int numTimes = 1)
+        {
+	        ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+
+            return View();
+        }
+    }
+}
